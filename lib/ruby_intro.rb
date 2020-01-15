872f00abc78183d4535025e36c016d754cef9920
yaramohamed98 <58967803+yaramohamed98@users.noreply.github.com>
2020-01-15 11:55:30 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,62 @@
-# When done, submit this entire file to the autograder.
+
+
+Programming Assignment 1
 
 # Part 1
 
+
 def sum arr
-  # YOUR CODE HERE
+	arr.sum
+      	# YOUR CODE HERE
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	arr.max(2).sum
 end
+  # YOUR CODE HERE
 
+# name the objects you're iterating over c 
 def sum_to_n? arr, n
+	arr.combination(2) { |c| return true if c.sum == n }
+	false
+
   # YOUR CODE HERE
 end
 
 # Part 2
 
 def hello(name)
+        'Hello, ' + name
   # YOUR CODE HERE
 end
 
 def starts_with_consonant? s
+        Regexp.new('^[qwrtypsdfghjklzxcvbnm]', Regexp:: IGNORECASE).match?(s)
   # YOUR CODE HERE
 end
 
 def binary_multiple_of_4? s
+        if /^[01]+$/.match?(s)
+                s.to_i(2) % 4 == 0
+
+        else
+                false
+        end
   # YOUR CODE HERE
 end
 
 # Part 3
 
 class BookInStock
+        attr_accessor :isbn, :price
+        def initialize isbn, price
+                raise ArgumentError if isbn.empty? || price <= 0
+                @isbn = isbn
+                @price = price
+        end
+        def price_as_string
+                format("$%.2f", @price)
+        end
+
 # YOUR CODE HERE
 end
